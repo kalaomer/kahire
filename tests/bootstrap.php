cf0a76c00a28b4892313fedb64c7db92a59fdf0e
Ömer KALA <kalaomer@hotmail.com>
2016-03-02 18:17:52 +0200

    init settings deleted

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,8 +2,4 @@
 
 define('TEST_DIR', __DIR__, true);
 
-ini_set('error_reporting', E_ALL); // or error_reporting(E_ALL);
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-
 include_once __DIR__.'/../vendor/autoload.php';
